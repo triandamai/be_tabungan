b28a6f086e1fdbc2bf0c70bef10e1bef64357c7a
Trian <triannurizkillah@gmail.com>
2021-03-17 08:49:06 -0400

    feat(fe): fe moved to standalone repo

--- fe_tabungan/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import WindiCSS from "vite-plugin-windicss";
-import Icons from "vite-plugin-icons";
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/tabungan/",
-  plugins: [vue(), WindiCSS(), Icons()],
-});
